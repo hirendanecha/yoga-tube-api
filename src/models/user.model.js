b82ca82e0c76a0fbbdb25101065f6d0577d0cc16
Swapnil Opash <sra.opash@gmail.com>
2024-05-20 11:50:51 +0530

    add email on user search

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -118,7 +118,7 @@ User.create = function (userData, result) {
 
 User.findAndSearchAll = async (limit, offset, search, startDate, endDate) => {
   let whereCondition = `u.IsAdmin != 'Y' ${
-    search ? `AND u.Username LIKE '%${search}%'` : ""
+    search ? `AND u.Username LIKE '%${search}%' OR u.Email LIKE '%${search}%'` : ""
   }`;
 
   if (startDate && endDate) {
